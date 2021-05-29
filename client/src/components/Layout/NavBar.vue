43c5618b9bdcc2352147ad47c2a7f969d44ef61a
DevinWalkup <51932866+DevinWalkup@users.noreply.github.com>
2021-05-29 13:43:36 -0700

    #6 - Mobile menu popup fix

--- client/src/components/Layout/NavBar.vue
+++ client/src/components/Layout/NavBar.vue
@@ -10,8 +10,8 @@
                    alt="Point Poker Logo"/>
             </a>
             <div class="-mr-2 flex items-center md:hidden">
-              <PopoverButton
-                  class="bg-primaryLight dark:bg-primaryDark rounded-md p-2 inline-flex items-center justify-center text-textLight dark:text-textDark hover:bg-primaryLightHover dark:hover:bg-primaryDarkHover focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
+              <PopoverButton @click="setOpenMenu"
+                             class="bg-primaryLight dark:bg-primaryDark rounded-md p-2 inline-flex items-center justify-center text-textLight dark:text-textDark hover:bg-primaryLightHover dark:hover:bg-primaryDarkHover focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                 <span class="sr-only">Open main menu</span>
                 <MenuIcon class="h-6 w-6" aria-hidden="true"/>
               </PopoverButton>
@@ -19,41 +19,49 @@
           </div>
           <div class="hidden space-x-8 md:flex md:ml-10">
             <router-link v-for="item in navigation" :key="item.name" :to="item.href"
-               class="text-base font-medium text-textLight dark:text-textDark hover:text-gray-300">{{ item.name }}</router-link>
+                         class="text-base font-medium text-textLight dark:text-textDark hover:text-gray-300">
+              {{ item.name }}
+            </router-link>
           </div>
         </div>
       </nav>
     </div>
 
+    <div v-show="openMenu">
     <transition enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 scale-95"
                 enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in"
                 leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-      <PopoverPanel focus class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
-        <div class="rounded-lg shadow-md bg-primaryLight dark:bg-primaryDark ring-1 ring-black ring-opacity-5 overflow-hidden">
-          <div class="px-5 pt-4 flex items-center justify-between">
-            <div>
-              <img class="h-20 w-auto" src="../../assets/PointPoker_Logo.png"
-                   alt="Point Poker Logo"/>
+
+        <PopoverPanel focus class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden z-30" static>
+          <div
+              class="rounded-lg shadow-md bg-primaryLight dark:bg-primaryDark ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div class="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <img class="h-20 w-auto" src="../../assets/PointPoker_Logo.png"
+                     alt="Point Poker Logo"/>
+              </div>
+              <div class="-mr-2">
+                <PopoverButton @click="setOpenMenu"
+                               class="bg-primaryLight dark:bg-primaryDark rounded-md p-2 inline-flex items-center justify-center text-textLight dark:text-textDark hover:bg-primaryLightHover dark:hover:bg-primaryDarkHover focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                  <span class="sr-only">Close menu</span>
+                  <XIcon class="h-6 w-6" aria-hidden="true"/>
+                </PopoverButton>
+              </div>
             </div>
-            <div class="-mr-2">
-              <PopoverButton
-                  class="bg-primaryLight dark:bg-primaryDark rounded-md p-2 inline-flex items-center justify-center text-textLight dark:text-textDark hover:bg-primaryLightHover dark:hover:bg-primaryDarkHover focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
-                <span class="sr-only">Close menu</span>
-                <XIcon class="h-6 w-6" aria-hidden="true"/>
-              </PopoverButton>
+            <div class="pt-5 pb-6">
+              <div class="px-2 space-y-1">
+                <router-link v-for="item in navigation" :key="item.name" :to="item.href" @click="setOpenMenu"
+                             class="block px-3 py-2 rounded-md text-base font-medium text-textLight dark:text-textDark hover:bg-primaryLightHover dark:hover:bg-primaryDarkHover">
+                  {{
+                    item.name
+                  }}
+                </router-link>
+              </div>
             </div>
           </div>
-          <div class="pt-5 pb-6">
-            <div class="px-2 space-y-1">
-              <router-link v-for="item in navigation" :key="item.name" :to="item.href"
-                 class="block px-3 py-2 rounded-md text-base font-medium text-textLight dark:text-textDark hover:bg-primaryLightHover dark:hover:bg-primaryDarkHover">{{
-                  item.name
-                }}</router-link>
-            </div>
-          </div>
-        </div>
-      </PopoverPanel>
+        </PopoverPanel>
     </transition>
+    </div>
   </Popover>
 </template>
 
@@ -83,11 +91,17 @@ export default {
         {name: 'Home', href: '/'},
         {name: 'Play', href: '/play'},
         {name: 'About', href: '/about'}
-      ]
+      ],
+      openMenu: false,
     }
   },
 
   methods: {
+    setOpenMenu() {
+      this.openMenu = !this.openMenu;
+    },
+
+
     sendHome() {
       this.$router.push('/');
     }
