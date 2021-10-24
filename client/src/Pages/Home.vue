3cad4465fb5fdcc60f27fa1763d773f3fe0e5f22
Devin <51932866+DevinWalkup@users.noreply.github.com>
2021-10-24 11:37:16 -0700

    Update Home.vue

--- client/src/Pages/Home.vue
+++ client/src/Pages/Home.vue
@@ -22,7 +22,7 @@
       </div>
       <div class="mt-12 -mb-16 sm:mb-48 lg:m-0 lg:relative">
         <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-          <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="../assets/PointPoker_Logo.png" alt="" />
+          <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="/PointPoker_Logo.png" alt="" />
         </div>
       </div>
     </div>
